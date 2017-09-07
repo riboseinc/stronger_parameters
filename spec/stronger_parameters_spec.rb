f703162240d0368005519bd600388f0f38ab9fd6
Sebastian Skałacki <skalee@gmail.com>
2017-09-07 20:33:52 +0200

    Improve existing autogenerated spec

--- spec/stronger_parameters_spec.rb
+++ spec/stronger_parameters_spec.rb
@@ -1,7 +1,8 @@
 require "spec_helper"
 
-RSpec.describe StrongerParameters do
+RSpec::describe StrongerParameters do
   it "has a version number" do
-    expect(StrongerParameters::VERSION).not_to be nil
+    expect(StrongerParameters::VERSION).to be_a(String)
+    expect(StrongerParameters::VERSION).not_to be_empty
   end
 end
